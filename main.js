6f8ce36ba46c631a06e3c2c676aba9881621f9f4
Aditi434 <82752546+Aditi434@users.noreply.github.com>
2022-04-06 15:48:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+status="";
+img="";
+objects=[];
+objectDetector="";
+
+
+
+function preload()
+{
+    img=loadImage('bedroom.jpg');
+}
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects.."
+}
+function modelLoaded()
+{
+    console.log('model has been loaded');
+    status = true;
+    objectDetector.detect(img, gotResults);
+
+}
+function gotResults(error, results)
+{
+if(error)
+{
+    console.log(error);
+}
+else{
+    console.log(results);
+    objects=results;
+}
+
+}
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    if (status!="")
+    {
+        for(var i=0;i<objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+
+            fill(255, 0, 0);
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(255, 0, 0);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
